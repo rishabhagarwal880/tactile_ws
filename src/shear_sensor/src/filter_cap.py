cd752ef21cbc67e1a3423c4a7807f692e3e05841
Rishabh Agarwal <rishabh@wireless-10-104-232-180.umd.edu>
2017-11-09 14:34:40 -0500

    filter for capcitance data added

--- src/shear_sensor/src/filter_cap.py
+++ src/shear_sensor/src/filter_cap.py
@@ -1,12 +1,41 @@
-from scipy import signal
+#!/usr/bin/env python
+import rospy
 import matplotlib.pyplot as plt
+from std_msgs.msg import String
+from std_msgs.msg import Float64
+from scipy import signal
+import builtins
 
-t = np.linspace(0, 1.0, 2001)
-xlow = np.sin(2 * np.pi * 5 * t)
-xhigh = np.sin(2 * np.pi * 250 * t)
-x = xlow + xhigh
+#create the list of capacitance
+cap_list=collections.deque(maxlen=20)
+capacitance = 1024
 
-b, a = signal.butter(8, 0.125)
-y = signal.filtfilt(b, a, x, padlen=150)
-np.abs(y - xlow).max()
+def callback(data):
+    rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.data)
+    global cap_list
+    global capacitance
+    cap_list.append(data.data)
+    if len(cap_list) < 20
+	capcitance = cap_list[-1] 
+    else
+       y= savgol_filter(cap_list, 15, 2)
+       capacitancce = y[-1]
+        
+def listener():
+
+    # In ROS, nodes are uniquely named. If two nodes with the same
+    # node are launched, the previous one is kicked off. The
+    # anonymous=True flag means that rospy will choose a unique
+    # name for our 'listener' node so that multiple listeners can
+    # run simultaneously.
+    rospy.init_node('filter', anonymous=True)
+    rospy.Subscriber("chatter", Float64, callback)
+    pub = rospy.Publisher('capacitance', Float64, queue_size=10)
+    rate = rospy.Rate(10) # 10hz
+    pub.publish(capacitance)
+    # spin() simply keeps python from exiting until this node is stopped
+    rospy.spin()
+
+if __name__ == '__main__':
+    listener()
 
